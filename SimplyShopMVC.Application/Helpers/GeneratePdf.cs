3cc8bcbc045284e1efa052fc3d8700ab7d4c76a3
JarekJJJ <jarek@hkr.net.pl>
2024-01-11 00:31:36 +0100

    - drobne poprawki

--- SimplyShopMVC.Application/Helpers/GeneratePdf.cs
+++ SimplyShopMVC.Application/Helpers/GeneratePdf.cs
@@ -16,30 +16,23 @@ namespace SimplyShopMVC.Application.Helpers
         public static byte[] GenertateOrderPdf(OrderFromCartVm orderFromCart)
         {
             var stream = new MemoryStream();
-
             var writer = new PdfWriter(stream);
             var pdf = new PdfDocument(writer);
             var document = new Document(pdf);
             AddImageToPdf(document, "wwwroot/media/primehkr24.png", 200, 64, 20, 750);
             document.Add(new Paragraph($"numer Zamówienia: {orderFromCart.orderForList.NumberOrders}").SetMarginTop(100).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER));
             document.Close();
-
             return stream.ToArray();
         }
         static void AddImageToPdf(Document document, string imagePath, float width, float height, float pozX, float pozY)
         {
             try
-            {
-                // Tworzenie obiektu Image na podstawie ścieżki do pliku obrazka
+            {              
                 iText.Layout.Element.Image image = new iText.Layout.Element.Image(ImageDataFactory.Create(imagePath));
-
                 // Ustawienie szerokości i wysokości obrazka
                 image.SetWidth(width);
-                image.SetHeight(height);
-
-                // Dodanie obrazka do lewego górnego rogu dokumentu
-                document.Add(image.SetFixedPosition(pozX, pozY)); // Dostosuj współrzędne X i Y według potrzeb
-
+                image.SetHeight(height);               
+                document.Add(image.SetFixedPosition(pozX, pozY)); 
             }
             catch (Exception ex)
             {
