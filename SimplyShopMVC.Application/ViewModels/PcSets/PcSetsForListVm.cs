ffe14921635758acc67badc357872db87d1f817d
JarekJJJ <jarek@hkr.net.pl>
2024-02-27 01:01:58 +0100

    - add vm pcset

---
+++ SimplyShopMVC.Application/ViewModels/PcSets/PcSetsForListVm.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using SimplyShopMVC.Application.Mapping;
+using SimplyShopMVC.Domain.Model.Sets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyShopMVC.Application.ViewModels.PcSets
+{
+    public class PcSetsForListVm : IMapFrom<SimplyShopMVC.Domain.Model.Sets.PcSets>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int GroupItemId { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<PcSetsForListVm, SimplyShopMVC.Domain.Model.Sets.PcSets>().ReverseMap();
+        }
+    }
+}
---
+++ SimplyShopMVC.Application/ViewModels/PcSets/SetsItemForListVm.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SimplyShopMVC.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyShopMVC.Application.ViewModels.PcSets
+{
+    public class SetsItemForListVm : IMapFrom<SimplyShopMVC.Domain.Model.Sets.PcSetsItems>
+    {
+        public int Id { get; set; }
+        public int PcSetsId { get; set; }
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal NetPurchasePrice { get; set; }
+        public int VatRateId { get; set; }
+        public int WarehouseId { get; set; }
+        public string EanCode { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<SetsItemForListVm, SimplyShopMVC.Domain.Model.Sets.PcSetsItems>().ReverseMap();
+        }
+
+    }
+}
